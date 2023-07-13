ad11cbc42d5a884abfbec9d95da1c4f03abd1863
prat112 <kundapurprathiksha@gmail.com>
2023-07-13 22:57:18 +0530

    secondbranch

---
+++ task1/main.js
@@ -0,0 +1,7 @@
+var headerTitle=document.getElementById('header-title');
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+
+var titleElement = document.querySelector('.title');
+titleElement.style.fontWeight = 'bold';
+titleElement.style.color = 'green';
